0f76b2b4467fd8bca124bf90e7fee581556e2744
rachcent <35813484+rachcent@users.noreply.github.com>
2019-09-29 23:06:37 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,28 +1,38 @@
 let randomIndex;
+let animating = false;
 
  function setup() {
    createCanvas(600, 600);
    background(200);
    textSize(32);
 
-   text("click to randomize",250,300)
+   text("click to randomize",250,300);
+   setTimeout(changeBackground,1000);
 
  }
- //hello
- //hi
+
  function draw() {
+   if( animating == true) {
+     ellipse(random(width),random(height), random(50,200));
+   }
+ }
+
+ function randomizer(){
+   if (students[0]){
+      //this displays random name and splices it out of array
+      background(247, 235, 173);
+      randomIndex = int(random(students.length));
+      text(students[randomIndex].lastName, 250, 300);
+      students.splice(randomIndex,1);
+   }  else{
+     background(247, 235, 173);
+     text("nothing left!", 250,300);
+   }
  }
 
  function mousePressed(){
+   animating = true;
+   setTimeout(randomizer, 2000);
 
-if (students[0]){
-   //this displays random name and splices it out of array
-   background(247, 235, 173);
-   randomIndex = int(random(students.length));
-   text(students[randomIndex].lastName, 250, 300);
-   students.splice(randomIndex,1);
-}  else{
-  background(247, 235, 173);
-  text("nothing left", 250,300);
-}
+
 }
